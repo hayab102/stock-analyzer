2533a0fd56d8e178005fa0c567bf397895ee7c72
hayab102 <hayab102@gmail.com>
2025-06-16 13:52:45 +0900

    Update get_all_tickers.py

--- get_all_tickers.py
+++ get_all_tickers.py
@@ -4,7 +4,7 @@ import pandas as pd
 url = "https://www.jpx.co.jp/markets/statistics-equities/misc/tvdivq0000001vg2-att/data_j.xls"
 
 # skiprows を適切に増やす（例：3〜5）
-df = pd.read_excel(url, skiprows=3)  # ← この数は調整の余地あり
+df = pd.read_excel(url, skiprows=0)  # ← この数は調整の余地あり
 print(df.columns)
 
 # 正しい列名が確認できたら、その列だけ抽出（例：'コード' ではなく '銘柄コード' かも）
